31a0535b72519c773a95bb62df6109fa1ed65e6a
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-02-06 03:31:13 +1100

    upload

---
+++ js/config.js
@@ -0,0 +1,180 @@
+//       _           _    _                     _ 
+//      | |         | |  | |                   | |
+//   ___| | ___  ___| | _| |     ___   __ _  __| |
+//  / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+//  \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+//  |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+//     version 2.16
+
+// URL for the server logo
+// Leave it blank to use text instead
+// (Put the files in the images folder)
+// (You can also use external URL)
+var l_serverImage = "https://cdn.discordapp.com/attachments/791543288620384286/791547370466181120/ay-large.png";
+
+// Center logo?
+// 'true' or 'false'
+var l_centerLogo = true;
+
+// Display current map and gamemode?
+// 'true' or 'false'
+var l_displayMapGamemode = false;
+
+// Custom server name configuration
+// Leave it blank for automatic server name
+var l_serverName = "Aystralia Network";
+
+// Use a video for the background?
+// Otherwise it will use images
+// 'true' or 'false'
+var l_bgVideo = true;
+
+// Default available videos:
+// "aurora.webm"
+// "bluefall.webm"
+// "bluetilefloor.webm"
+// "bluewhitecircles.webm"
+// "bokehlens.webm"
+// "cleanbokeh.webm"
+// "coldnight.webm"
+// "colorfulburst.webm"
+// "dancingstars.webm"
+// "dropsflares.webm"
+// "focusorangebokeh.webm"
+// "glimmering.webm"
+// "glowingrings.webm"
+// "greencircles.webm"
+// "hues.webm"
+// "inthedeep.webm"
+// "modernflourish.webm"
+// "orangeaura.webm"
+// "warminside.webm"
+// "yellowgreen.webm"
+//
+// You can add your own video backgrounds
+// Just make sure they are using webm VP8 format 
+// (Place the videos in the backgrounds/videos folder)
+// (You can also use external URL)
+var l_background = "coldnight.webm";
+
+// (Image-background only)
+// Enable map-based background image?
+// If enabled, the background will be an image based on the server current map
+// You need to have a background image with the same name as the map
+// Also, the image needs to be in .jpg format
+// E.g: gm_flatgrass.jpg, gm_construct.jpg, rp_downtown_v4c_v2.jpg
+// 'true' or 'false'
+var l_bgImageMapBased = false;
+
+// (Image-background only)
+// Place one or more images
+// Multiple images will automatically shuffle between them
+// (Place the files in the backgrounds/images folder)
+// (You can also use external URL)
+var l_bgImages = [
+	"touhou01.jpg",
+	"touhou02.jpg",
+	"touhou03.jpg"
+];
+
+// (Image-background only)
+// Random background images order?
+// 'true' or 'false'
+var l_bgImagesRandom = false;
+
+// (Image-background only)
+// Delay between background images changes
+// in milliseconds
+var l_bgImageDuration = 5000;
+
+// (Image-background only)
+// Image background fade velocity
+// in milliseconds
+var l_bgImageFadeVelocity = 2000;
+
+// Enable background overlay?
+// 'true' or 'false'
+var l_bgOverlay = true;
+
+// Background darkening amount
+// 0 for none (0%), 100 for pitch black (100%)
+var l_bgDarkening = 0;
+
+// Play music during the loading screen?
+// 'true' or 'false'
+var l_music = true;
+
+// Display current music name?
+// 'true' or 'false'
+var l_musicDisplay = false;
+
+// Music playlist
+// Add as many youtube ids/urls or ogg files you want
+// (Place the ogg files in the music folder)
+// (You can also use external URL for the ogg files)
+var l_musicPlaylist = [
+	{ogg: "menatwork.ogg", name: "GTA IV Loading..."},
+];
+
+// Random music order?
+// 'true' or 'false'
+var l_musicRandom = false;
+
+// Music volume
+// Choose a value between 0 (quietest) and 100 (loudest)
+var l_musicVolume = 5;
+
+// Enable custom messages?
+// 'true' or 'false'
+var l_messagesEnabled = true;
+
+// Enter your custom messages below
+var l_messages = [
+	"Press G, trust me",
+	"Use !rules in-game",
+	"If you're seeing errors use !content",
+	"Use 64 bit Garry's Mod if you're stuck on 'Starting Lua'",
+	"We have a cinema!",
+	"Best way to start making money would be as a Lean Producer",
+	"Pressing R or Right Clicking on Arrest/Unaresst Batton will change its property",
+	"The prop limit is 50 :( If you need a prop unblacklisted let the staff know",
+	"Rob the PD once in a while, its worth it",
+	"Drop a loading music suggestion in #suggestions",
+	"Garry's Mod runs like shit",
+	"We change the loading music once a week!",
+	"We once banned a staff member for skipping school to play Garry's Mod",
+	"OVH aren't very good, typical french people",
+	"I miss french guy running to reboot the servers back in the day",
+	"We used to own an old RP server called Exalted RP, what a disaster",
+	"Aystralia Network was founded on the 24/12/2020",
+	"If you have anything we can add to the server, let us know in the discord",
+	"We are kind of like supremacy's better half, way more fun",
+	"If you're interested, we are planning on hosting other game servers too",
+	"Boost our discord upon joining the server for 6 million dollars",
+];
+
+// Random message order?
+// 'true' or 'false'
+var l_messagesRandom = true;
+
+// Delay between message changes
+// in milliseconds
+var l_messagesDelay = 5000;
+
+// Messages fade time
+// in milliseconds
+var l_messagesFade = 1000;
+
+// Show errors inside Garry's Mod?
+// Can be useful for finding errors
+// 'true' or 'false'
+var l_showErrorsIngame = false;
+
+//==============================================================
+//========================== WARNING ===========================
+//==============================================================
+//============ Do not edit anything below this line ============
+//==============================================================
+var checkConfigFile = function() {
+	return true;
+};
